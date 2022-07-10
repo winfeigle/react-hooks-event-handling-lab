558f754c96f2a1311d2fd5ab82f31671c68c9ca1
Win Feigle <win.feigle@gmail.com>
2022-07-10 10:03:27 -0600

    Pass Assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,12 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe(){
+    return(
+    <div>
+        <button onFocus={() => console.log('Good!')} onBlur={() => console.log('Hey! Eyes on me!')}>Eyes on me</button>
+    </div>
+    );
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,12 @@
 // Code Keypad Component Here
+import React from "react";
+
+function Keypad(){
+    return(
+        <div>
+            <input onChange={() => console.log('Entering password...')} type="password" />
+        </div>
+    );
+}
+
+export default Keypad;
